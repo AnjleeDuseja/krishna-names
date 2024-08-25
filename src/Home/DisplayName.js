434b803897b3ab16fcd0100b3a8802aad5557058
Anjlee Duseja <anjleeduseja13@gmail.com>
2024-08-26 03:42:57 +0500

    added names

--- src/Home/DisplayName.js
+++ src/Home/DisplayName.js
@@ -5,21 +5,30 @@ import bannerImage from "../assets/banner.jpg"
 function DisplayName() {
    
     const {id}=useParams();
-    const name=data.names.filter(element=> element.name.includes(id))
+
+    function isName(dataName) {
+      return dataName.name === id;
+    }
+    const name=data.names.find(isName)
    
     
     return (
+      <>
 <div className="flex flex-col items-center ">
    <div>
 <img className="h-50 w-50 " src={bannerImage} alt="" />
 </div>
 <div >
    <p className="text-4xl font-bold text-gray-700 capitalize bg-slate-200 py-2 px-5 shadow-lg border-slate-900">
- {name[0].name}
+ {name.name}
    </p>
    </div>
-   <p className="box-border p-4 text-xl italic line-clamp-3 text-teal-950 font-underline font-sans"> {name[0].meaning}</p>
+   <p className="box-border p-4 text-xl italic line-clamp-3 text-teal-950 font-underline font-sans"> {name.meaning}</p>
+  
    </div>
+   
+    </>
+
     );
   }
   
