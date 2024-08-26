ac29f5da34f3863e555c16e6f55f008cdbbbde87
Anjlee Duseja <anjleeduseja13@gmail.com>
2024-08-26 20:29:16 +0500

    updated names

--- src/Home/DisplayName.js
+++ src/Home/DisplayName.js
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import data from "../krishnaNames.json"
+import data from "../krishnaNamesU.json"
 import bannerImage from "../assets/banner.jpg"
 
 import matkiImage from "../assets/matki.jpg"
@@ -13,13 +13,13 @@ function DisplayName() {
       return dataName.name === id;
     }
 
-    // function generateLinks(){
-    //   const LinkArr=data.names.map((ele)=>{return "https://krishna-name.vercel.app/"+ele.name});
-    //   console.log(LinkArr)
+    function generateLinks(){
+      const LinkArr=data.names.map((ele)=>{return "https://krishna-name.vercel.app/"+ele.name});
+      console.log(LinkArr)
 
-    // }
+    }
     
-    // generateLinks();
+    generateLinks();
 
 
 
@@ -52,3 +52,30 @@ function DisplayName() {
   }
   
   export default DisplayName;
+
+
+
+
+//   51. Sarpana  The one who surrenders to all (or causes surrender).
+// 52. Sagar  The ocean, symbolizing vastness.
+// 53. Mohan  The enchanter.
+// 54. Mukund  The giver of liberation.
+// 55. Purushottam  The supreme being or the best among men.
+// 56. Neelmani  The blue jewel.
+// 57. Suryakant  The beloved of the sun.
+// 58. Lajpal  The protector of honor.
+// 59. Govind  The protector of cows and the senses.
+// 60. Amrit  The immortal one.
+// 61. Priyakant  The beloved one.
+// 62. Sarvdata  The giver of everything.
+// 63. Chakradhar  The bearer of the discus (Sudarshan Chakra).
+// 64. Kans Sudhan  The slayer of Kansa.
+// 65. Nandanandan  The son of Nanda.
+// 66. Jagatpita  The father of the universe.
+// 67. Parthasarathi  The charioteer of Arjuna (Partha).
+// 68. Abhay  The fearless one.
+// 69. Vaman  The dwarf incarnation (Vamana avatar).
+// 70. Sarvanand  The source of all bliss.
+// 71. Ranchod  The one who left the battlefield (as part of a divine strategy).
+// 72. Prandata  The giver of life.
+// 73. Manharan  The stealer of hearts.
